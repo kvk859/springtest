12f18ca865bc56aa5fea03dd9b25f91060882735
iamvinay88 <107233421+iamvinay88@users.noreply.github.com>
2022-09-29 20:53:34 +0530

    Update DemoRestController.java

--- src/main/java/com/javatpoint/DemoRestController.java
+++ src/main/java/com/javatpoint/DemoRestController.java
@@ -1,20 +1,22 @@
 package com.javatpoint;
+package com.crunchify.tutorials;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-package com.crunchify.tutorials;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 @RestController
 public class DemoRestController 
 {
 @GetMapping("/D3V")
-	//public ModelAndView index(HttpServletRequest request) {
+	//public ModelAndView index(HttpServletRequest request) 
 public String hello() 
 {
-	InetAddress ip;
+	//InetAddress ip;
 	
-	ip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
+//	ip = InetAddress.getLocalHost();
+           // hostname = ip.getHostName();
             //System.out.println("Your current IP address : " + ip);
           //  System.out.println("Your current Hostname : " + hostname);
   
