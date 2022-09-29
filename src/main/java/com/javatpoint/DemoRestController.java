c5e67c04c97fec93adb1ac21d0db6a2f4b63e3a8
iamvinay88 <107233421+iamvinay88@users.noreply.github.com>
2022-09-29 20:13:20 +0530

    Update DemoRestController.java

--- src/main/java/com/javatpoint/DemoRestController.java
+++ src/main/java/com/javatpoint/DemoRestController.java
@@ -1,4 +1,4 @@
-package com.javatpoint;
+ /*package com.javatpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -33,4 +33,32 @@ public class DemoRestController
 		model.addObject("clientIp", clientIp);
 		return model;
 	}
+}
+*/
+
+package dev.simplesolution.ip.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import dev.simplesolution.ip.service.RequestService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private RequestService requestService;
+	
+	@RequestMapping("/")
+	public ModelAndView index(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("index");
+		String clientIp = requestService.getClientIp(request);
+		model.addObject("clientIp", clientIp);
+		return model;
+	}
+
 }
